48b1819bb4851e9e8f76f447d217652b1105944e
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2020-01-10 15:50:16 -0700

    sr

---
+++ transition_system.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+
+class TransitionSystem:
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.buffer = []
+        self.stack = []
+
+    def is_done(self):
+        return len(self.buffer) == 0 and len(self.stack) == 1
+
+    def get_result(self):
+        if self.is_done():
+            return self.stack[0]
+
+    def shift(self):
+        node = self.buffer.pop(0)
+        self.stack.append(node)
+
+    def reduce(self):
+        rhs = self.stack.pop()
+        lhs = self.stack.pop()
+        node = Node(lhs, rhs)
+        self.stack.append(node)
+
+    def all_actions(self):
+        return ['shift', 'reduce']
+
+    def valid_actions(self):
+        actions = []
+        if self.can_shift():
+            actions.append('shift')
+        if self.can_reduce():
+            actions.append('reduce')
+        return actions
+
+    def can_shift(self):
+        return len(self.buffer) >= 1
+
+    def can_reduce(self):
+        return len(self.stack) >= 2
