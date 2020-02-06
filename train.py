388d46e5c4e1208b6b9eb354286915cf1a3840cf
bsharpataz <bsharp@email.arizona.edu>
2020-02-06 14:27:30 -0700

    output format

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ def predict(data_dir, model_dir):
     all_gold_nodes = []
     all_pred_nodes = []
     for annotation in tqdm(load_annotations(data_dir)):
-        _, pred_tree = discobert(annotation.edus)
+        pred_tree = discobert(annotation.edus)[0]
         all_gold_nodes.extend(annotation.dis.get_nonterminals())
         all_pred_nodes.extend(pred_tree.get_nonterminals())
 
