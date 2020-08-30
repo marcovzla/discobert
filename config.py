27bbf9eb2cc342c6c424376e4dceca59a71cdc77
maxaalexeeva <maxaalexeeva@gmail.com>
2020-08-30 10:29:45 -0700

    before the sorted input experiment

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import tokenizers
 from transformers import *
 
-ENCODING = 'bert' 
+ENCODING = 'xlnet' 
 DEBUG = False # no saving of files; output in the terminal; first random seed from the list
 RERUN_DEV_EVAL = False # True to rerun eval on the same dev sets that were used during training
-EXPERIMENT_ID = 10
-EXPERIMENT_DESCRIPTION = f"{ENCODING}-two-classifier-only-stack-for-label-classifier-drop-cls-with-attention" # during training: enter a brief description that will make the experiment easy to identify #during testing: this is the name of the parent directory for different random seed models saved from an experiment
+EXPERIMENT_ID = 11
+EXPERIMENT_DESCRIPTION = f"{ENCODING}-two-classifier-only-stack-for-label-classifier-sorted-input" # during training: enter a brief description that will make the experiment easy to identify #during testing: this is the name of the parent directory for different random seed models saved from an experiment
 TEST_SIZE = 0.15 #If float, should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split. If int, represents the absolute number of test samples. If None, the value is set to the complement of the train size. If train_size is also None, it will be set to 0.25. (https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html)
 EPOCHS = 30
 MAX_LEN = 50
@@ -21,9 +21,9 @@ DIRECTION_HIDDEN_SIZE = 10
 
 INCLUDE_RELATION_EMBEDDING = False
 INCLUDE_DIRECTION_EMBEDDING = False #has to be false for the two classifier version
-USE_ATTENTION = True
-DROP_CLS = True #whether or not drop the beginning of sequence token (bos_token)
-SORT_INPUT = False #simplified curriculum learning
+USE_ATTENTION = False
+DROP_CLS = False #whether or not drop the beginning of sequence token (bos_token)
+SORT_INPUT = True #simplified curriculum learning
 
 
 DISCOBERT_PATH = Path('~/data/discobert').expanduser() 
