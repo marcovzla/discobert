e57a6a4032ab91f8e434ca89d06fcdaa72b4a0e1
Maria <maxaalexeeva@gmail.com>
2020-09-05 02:07:15 -0700

    some clean up

--- segmenter_model.py
+++ segmenter_model.py
@@ -67,17 +67,13 @@ class SegmentationModel(nn.Module):
             gold_as_tokens = [] # corresponding tokens (for debugging)
 
             for i in range(len(edus)):
-                # print(edus[i])
+
                 tokenized_edu = self.tokenizer(edus[i])
                 ids.extend(tokenized_edu.input_ids[1:])
-                # attention_mask.extend(tokenized_edu.attention_mask[1:])
-                # token_type_ids.extend(tokenized_edu.token_type_ids[1:])
-                
+                 
                 for j in range(len(tokenized_edu.input_ids)): 
                     token_id = tokenized_edu.input_ids[j]
-                    # print("token id: ", token_id)
                     token = self.tokenizer.convert_ids_to_tokens([token_id])
-                    # print("token: ", token)
                     
                     if token[0] != "[SEP]" and token[0] != "[CLS]":
                         # print("tok added to gold: ", token[0])
@@ -101,19 +97,15 @@ class SegmentationModel(nn.Module):
         # encoding the sentences
         # fixme: 
 
-        predictions = []
-        
+        predictions = []        
         losses = []
         potential_edu = [] # only needed for the run mode (only return new annotations, no eval)
         new_edus = []
 
-        sentence_tokens_full_doc = []
-        sent_gold_preds = []
         current_gold = gold_tags
-        encoded_sentences = []
+
         predictions_based_on_sent = []
-        tokens_to_classify = [] # here will go the tokens that will be classified as B or I; excludes certain special tokens (model-specific; for bert, [CLS] and [SEP]---but not [UNK]); used for building new edus and debugging 
-        tokens_to_classify_bertified = [] # here will be the bert encodings of the classifiable tokens
+
         for sent in sentences:
             tokenized_sent_not_tensored = self.tokenizer(sent) # getting tokens from here #fixme: this and next lines need to be unified somehow
             tokenized_sent = self.tokenizer(sent, return_tensors='pt') # getting encodings using this
@@ -129,58 +121,50 @@ class SegmentationModel(nn.Module):
             encoded_sent, pooled_output = self.encoder(ids, attention_mask, token_type_ids)
             # print('ecnoded sent: ', encoded_sent.shape)
             sent_encoded_only_classifiable_tokens = []
+            
+            sent_only_classifiable_tokens = []
             # getting classifiable tokens (losing special tokens); squeeze each sent bc for 1 sentence, the 0th dim is 1
             # fixme: get rid of squeezing redundancy
-            sent_only_classifiable_tokens = []
             for i in range(encoded_sent.squeeze(dim=0).shape[0]): 
-                # print("tok sent input ids: ", tokenized_sent.input_ids[0])
-                # print("cur token id: ", tokenized_sent.input_ids[0][i])
-                # print("token in sent: ", sent_tokens[i])
                 if sent_tokens[i] != '[SEP]' and sent_tokens[i] != '[CLS]':
                     if i == 1:
                         predictions_based_on_sent.append("B")
                     else:
                         predictions_based_on_sent.append("I")
                     sent_only_classifiable_tokens.append(sent_tokens[i])
-                    tokens_to_classify.append(sent_tokens[i])
+                    # tokens_to_classify.append(sent_tokens[i])
                     token_encoding = encoded_sent.squeeze(dim=0)[i].unsqueeze(dim=0)
                     # print("token encoding: ", token_encoding.shape)
-                    tokens_to_classify_bertified.append(token_encoding)
+                    # tokens_to_classify_bertified.append(token_encoding)
                     sent_encoded_only_classifiable_tokens.append(token_encoding)
             
+            # the first len(sent) predictions are for the current sentence
             gold_for_sent = current_gold[:len(sent_encoded_only_classifiable_tokens)]
-            # sent_gold_preds.append(gold_for_sent)
+            #update 'current_gold', i.e., we have gotten the gold predictions for this sentence, so we can remove them from
+            # the gold labels list---the next batch of those is for the next sentence
             current_gold = current_gold[len(sent_encoded_only_classifiable_tokens):]
             # encoded_sentences.append(sent_encoded_only_classifiable_tokens)
             # sentence_tokens_full_doc.append(sent_only_classifiable_tokens)
             num_of_tokens_in_sent = len(sent_encoded_only_classifiable_tokens)
+
+            # go through all the encoded tokens in the sentence
             for j in range(len(sent_encoded_only_classifiable_tokens)):
-        # for i in range(tokens_to_classify_bertified.shape[0]):
-
+                # get classifier scores
                 prediction_scores = self.segment_classifier(sent_encoded_only_classifiable_tokens[j])
-
-                # print("pred: ", prediction_scores.shape)
-                # print("max: ", torch.argmax(prediction_scores))
                 predicted_tag = self.id_to_segmenter_tag[torch.argmax(prediction_scores)]
-                # if train == False:
-                #     gold_pred = gold_tags[i]
-                #     print("token, prediction, gold: ", predicted_tag, " ", tokenizer.convert_ids_to_tokens([ids.squeeze(dim=0)[i]]), " ", gold_pred)
-            
+                
                 # are we training?
                 if mode == "train":
-                    # print("pred tag: ", predicted_tag)
-                    # print("i line 187: ", i)
-                    # all_labels = sent_gold_preds[i]
+                    
                     gold_pred_label = gold_for_sent[j]
                     pred_id = self.segmenter_tag_to_id[gold_pred_label]
-                    # print("pred label, id: ", gold_pred_label, " ", pred_id)
                     gold_pred = torch.tensor([pred_id], dtype=torch.long).to(self.device)
-                    # print("gold pred: ", gold_pred)
-                    # predictions.append(gold_tags[i])
                     # if self.id_to_segmenter_tag[gold_pred] != predicted_tag:
                     #     print("WRONG PRED: ", self.id_to_segmenter_tag[gold_pred], " ", predicted_tag, " ", tokenizer.convert_ids_to_tokens([ids.squeeze(dim=0)[i]]))
                     # else:
                     #     print("CORRECT PRED: ", self.id_to_segmenter_tag[gold_pred], " ", predicted_tag, " ", tokenizer.convert_ids_to_tokens([ids.squeeze(dim=0)[i]]))
+                    
+                    # prob dont need this in train?
                     predictions.append(predicted_tag)
                     
                     loss_on_tags = loss_fn(prediction_scores, gold_pred)
@@ -232,38 +216,6 @@ class SegmentationModel(nn.Module):
                         potential_edu.append(token)
                 
             
-            
-            
-            
-            
-            
-        # for i in range(len(tokens_to_classify)):
-        #     print(tokens_to_classify[i], " ", predictions_based_on_sent[i])
-        # for i in range(len(sentence_tokens_full_doc)):
-        #     for j in range(len(sentence_tokens_full_doc[i])):
-        #         print("token: ", sentence_tokens_full_doc[i][j], " pred: ", sent_gold_preds[i][j])
-        # this will be the whole document as token embeddings (only the classifiable ones)
-        # tokens_to_classify_bertified = torch.cat(tokens_to_classify_bertified, dim=0)
-        # print("shape tokens to classify bertified: ", tokens_to_classify_bertified.shape)
-
-        # for sent in encoded_sentences:
-        #     for token in sent:
-        #         print(token.shape)
-
-        # print("sent gold pred: ", sent_gold_preds)
-        
-        
-        
-
-        # for i in range(len(encoded_sentences)):
-        #     # print("i: ", i)
-        #     num_of_tokens_in_sent = len(encoded_sentences[i])
-            
-            
-        #this is in case i want to compare old and new edus; breaks if there are more new edus than old ones, which is fine for my purposes here
-        # for i in range(len(new_edus)):
-        #     print("========\nold edu: ", annotation.edus[i])
-        #     print("new edu: ", new_edus[i])
             
 
         # are we training?
