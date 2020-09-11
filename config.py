078610b2ed14a8c5cd15a7dae4c886d0bc4550ba
maxaalexeeva <maxaalexeeva@gmail.com>
2020-09-11 00:01:52 -0700

    after roberta drop cls with att experiment

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import tokenizers
 from transformers import *
 
-ENCODING = 'xlnet' 
+ENCODING = 'roberta' 
 DEBUG = False # no saving of files; output in the terminal; first random seed from the list
 RERUN_DEV_EVAL = False # True to rerun eval on the same dev sets that were used during training
-EXPERIMENT_ID = 13
-EXPERIMENT_DESCRIPTION = f"experiment13-xlnet-two-classifier-only-stack-for-label-classifier-drop-cls-with-att-2020-09-09" # during training: enter a brief description that will make the experiment easy to identify #during testing: this is the name of the parent directory for different random seed models saved from an experiment
+EXPERIMENT_ID = 14
+EXPERIMENT_DESCRIPTION = f"{ENCODING}-two-classifier-only-stack-for-label-classifier-drop-cls-with-att" # during training: enter a brief description that will make the experiment easy to identify #during testing: this is the name of the parent directory for different random seed models saved from an experiment
 TEST_SIZE = 0.15 #If float, should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split. If int, represents the absolute number of test samples. If None, the value is set to the complement of the train size. If train_size is also None, it will be set to 0.25. (https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html)
 EPOCHS = 30
 MAX_LEN = 50
